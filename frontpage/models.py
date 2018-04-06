323dd205ba35c63c84bdc8d3632d2f4265d035f3
molly <mollywilson_@outlook.com>
2018-04-06 17:05:00 +0100

    completed tutorial 8

--- frontpage/models.py
+++ frontpage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Earrings(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    picture = models.CharField(max_length=1000)
+
+
+class Rings(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    picture = models.CharField(max_length=1000)
+    size = models.CharField(max_length=50)
+    collection = models.ForeignKey(Earrings, on_delete=models.CASCADE)
--- frontpage/views.py
+++ frontpage/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("<h1> This is the frontpage app homepage </h1>")
